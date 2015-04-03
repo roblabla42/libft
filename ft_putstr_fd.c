0794abaca257ad7d535d39c23378f4d84918bf74
roblabla <robinlambertz+dev@gmail.com>
2015-04-03 19:03:31 +0200

    Oops

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,10 +6,11 @@
 /*   By: rlambert <rlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 16:39:02 by rlambert          #+#    #+#             */
-/*   Updated: 2015/04/03 19:01:03 by rlambert         ###   ########.fr       */
+/*   Updated: 2015/04/03 19:03:18 by rlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
 
 void	ft_putstr_fd(char const *s, int fd)
